0a1b0f079af7daa2331830725e1ae97628b80c80
Huz Maryna <maryna.huz.dev@gmail.com>
2025-03-31 16:20:01 +0300

    Add CategoryType to ExpenseCategory for better classification

--- src/main/java/com/softserve/model/category/ExpenseCategory.java
+++ src/main/java/com/softserve/model/category/ExpenseCategory.java
@@ -2,36 +2,42 @@ package com.softserve.model.category;
 
 public enum ExpenseCategory implements Category {
 
-    FOOD("Food"),
-    DINING_OUT("Dining Out"),
-    TRANSPORTATION("Transportation"),
-    TAXI("Taxi"),
-    RENT("Rent"),
-    UTILITIES("Utilities"),
-    ENTERTAINMENT("Entertainment"),
-    GAMES("Games"),
-    HEALTHCARE("Healthcare"),
-    EDUCATION("Education"),
-    BOOKS("Books"),
-    SHOPPING("Shopping"),
-    TRAVEL("Travel"),
-    MISCELLANEOUS("Miscellaneous"),
-    SUBSCRIPTIONS("Subscriptions"),
-    GIFTING("Gifting"),
-    HOME_IMPROVEMENT("Home Improvement"),
-    PERSONAL_CARE("Personal Care"),
-    TAXES("Taxes"),
-    CHARITY("Charity"),
-    HOBBY("Hobby");
+    FOOD("Food", CategoryType.EXPENSE),
+    DINING_OUT("Dining Out", CategoryType.EXPENSE),
+    TRANSPORTATION("Transportation", CategoryType.EXPENSE),
+    TAXI("Taxi", CategoryType.EXPENSE),
+    RENT("Rent", CategoryType.EXPENSE),
+    UTILITIES("Utilities", CategoryType.EXPENSE),
+    ENTERTAINMENT("Entertainment", CategoryType.EXPENSE),
+    GAMES("Games", CategoryType.EXPENSE),
+    HEALTHCARE("Healthcare", CategoryType.EXPENSE),
+    EDUCATION("Education", CategoryType.EXPENSE),
+    BOOKS("Books", CategoryType.EXPENSE),
+    SHOPPING("Shopping", CategoryType.EXPENSE),
+    TRAVEL("Travel", CategoryType.EXPENSE),
+    MISCELLANEOUS("Miscellaneous", CategoryType.EXPENSE),
+    SUBSCRIPTIONS("Subscriptions", CategoryType.EXPENSE),
+    GIFTING("Gifting", CategoryType.EXPENSE),
+    HOME_IMPROVEMENT("Home Improvement", CategoryType.EXPENSE),
+    PERSONAL_CARE("Personal Care", CategoryType.EXPENSE),
+    TAXES("Taxes", CategoryType.EXPENSE),
+    CHARITY("Charity", CategoryType.EXPENSE),
+    HOBBY("Hobby", CategoryType.EXPENSE);
 
     private final String categoryName;
+    private final CategoryType categoryType;
 
-    ExpenseCategory(String categoryName) {
+    ExpenseCategory(String categoryName, CategoryType categoryType) {
         this.categoryName = categoryName;
+        this.categoryType = categoryType;
     }
 
     @Override
     public String getCategoryName() {
         return categoryName;
+    }
+
+    public CategoryType getCategoryType() {
+        return categoryType;
     }
 }
