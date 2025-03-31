3d75d05d894b9d9a3594cd43a519f1382b927041
Huz Maryna <maryna.huz.dev@gmail.com>
2025-03-31 15:54:55 +0300

    Enhance IncomeCategory enum with CategoryType field

--- src/main/java/com/softserve/model/category/IncomeCategory.java
+++ src/main/java/com/softserve/model/category/IncomeCategory.java
@@ -2,22 +2,28 @@ package com.softserve.model.category;
 
 public enum IncomeCategory implements Category {
 
-    SALARY("Salary"),
-    FREELANCE("Freelance"),
-    BONUS("Bonus"),
-    GIFTS("Gifts"),
-    INVESTMENTS("Investments"),
-    RENTAL_INCOME("Rental Income"),
-    OTHER_INCOME("Other Income");
+    SALARY("Salary", CategoryType.INCOME),
+    FREELANCE("Freelance", CategoryType.INCOME),
+    BONUS("Bonus", CategoryType.INCOME),
+    GIFTS("Gifts", CategoryType.INCOME),
+    INVESTMENTS("Investments", CategoryType.INCOME),
+    RENTAL_INCOME("Rental Income", CategoryType.INCOME),
+    OTHER_INCOME("Other Income", CategoryType.INCOME);
 
     private final String categoryName;
+    private final CategoryType categoryType;
 
-    IncomeCategory(String categoryName) {
+    IncomeCategory(String categoryName, CategoryType categoryType) {
         this.categoryName = categoryName;
+        this.categoryType = categoryType;
     }
 
     @Override
     public String getCategoryName() {
         return categoryName;
+    }
+
+    public CategoryType getCategoryType() {
+        return categoryType;
     }
 }
