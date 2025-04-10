c0c8c97a32d058b6a956d276c9abe9abc0d6bb6c
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-10 21:59:38 +0300

    Add Lombok annotations and description field to Transaction
    Introduced @NoArgsConstructor and @EqualsAndHashCode annotations to simplify code and ensure proper handling of equals/hashCode methods. Also, added a new "description" field to enhance transaction details. Adjusted field order for better readability.

--- src/main/java/com/softserve/models/Transaction.java
+++ src/main/java/com/softserve/models/Transaction.java
@@ -2,21 +2,23 @@ package com.softserve.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.softserve.models.category.Category;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Transaction {
 
     private String transactionId;
 
-    private Category category;
-    private double transactionAmount;
-
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate transactionDate;
-
+    private Category category;
+    private double transactionAmount;
+    private String description;
 
     private static int idCounter = 100;
-
 
 }
