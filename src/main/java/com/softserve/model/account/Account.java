5137d0340054382ed3f53b7670f1244f77a61def
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-10 10:15:38 +0300

    Add transactions list and constructor to Account class

--- src/main/java/com/softserve/model/account/Account.java
+++ src/main/java/com/softserve/model/account/Account.java
@@ -1,11 +1,21 @@
 package com.softserve.model.account;
 
+import com.softserve.model.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
 
     private String name;
-    private Currency currency;
+    private final Currency currency;
     private double balance;
+    private List<Transaction> transactions;
 
-
-
+    public Account(String name, Currency currency, double balance) {
+        this.name = name;
+        this.currency = currency;
+        this.balance = balance;
+        this.transactions = new ArrayList<>();
+    }
 }
