3fcaf2a78b6728a291ac7afb16f17b6a802683b0
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-10 14:59:25 +0300

    Add generic DAO interface
    Introduce a generic DAO interface to standardize CRUD operations. This will enhance code reusability and simplify working with different entities in the application.

---
+++ src/main/java/com/softserve/dao/DAO.java
@@ -0,0 +1,13 @@
+package com.softserve.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DAO<T> {
+
+    void save(T entity);
+    Optional<T> read(String entity);
+    List<T> readAll();
+    void update(T entity);
+    void delete(T entity);
+}
