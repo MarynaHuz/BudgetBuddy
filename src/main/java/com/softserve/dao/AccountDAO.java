a1da49ffee44a2f4cd5d13e379ee3f386588ba67
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-10 14:22:12 +0300

    Use Optional for findByName in AccountDAO
        Updated the return type of findByName to Optional to better handle cases where no account is found. This change improves null safety and aligns with best practices for handling potentially absent values.

--- src/main/java/com/softserve/dao/AccountDAO.java
+++ src/main/java/com/softserve/dao/AccountDAO.java
@@ -3,12 +3,13 @@ package com.softserve.dao;
 import com.softserve.models.account.Account;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountDAO {
 
     void saveAccount(Account account);
 
-    Account findByName(String accountName);
+    Optional<Account> findByName(String accountName);
 
     boolean existsByName(String accountName);
 
