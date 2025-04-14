a08524e18cab6770b2dec9921460f2ffafb357ac
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-14 15:28:41 +0300

    Refactor Account model to enhance validation and structure
    Replaced primitive types with proper classes for better data handling. Introduced validation for currency and initial balance inputs. Added a unique ID generation for each account and renamed variables for improved clarity.

--- src/main/java/com/softserve/models/account/Account.java
+++ src/main/java/com/softserve/models/account/Account.java
@@ -3,28 +3,32 @@ package com.softserve.models.account;
 import com.softserve.models.Transaction;
 import lombok.*;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.softserve.models.validators.AccountValidation.validateCurrency;
+import static com.softserve.models.validators.AccountValidation.validateInitialBalance;
 
 @NoArgsConstructor
 @Getter
 @EqualsAndHashCode
 public class Account {
 
-    private String name;
+    private static int nextId = 1;
+
+    private long id;
+    @Setter
+    private String accountName;
     private Currency currency;
-    private double balance;
+    private BigDecimal initialBalance;
     private List<Transaction> transactions;
 
-    public Account(String name, Currency currency, double balance) {
-        this.name = name;
-        this.currency = currency;
-        this.balance = balance;
+    public Account(String accountName, String currency, String initialBalance) {
+        this.id = nextId++;
+        this.accountName = accountName;
+        this.currency = validateCurrency(currency);
+        this.initialBalance = validateInitialBalance(initialBalance);
         this.transactions = new ArrayList<>();
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
