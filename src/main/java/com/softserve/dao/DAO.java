46f440fea7a268266e320692d261adec3f614871
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-14 12:24:16 +0300

    Rename readAll() method to getAll() in DAO interface

--- src/main/java/com/softserve/dao/DAO.java
+++ src/main/java/com/softserve/dao/DAO.java
@@ -7,7 +7,7 @@ public interface DAO<T> {
 
     void save(T entity);
     Optional<T> read(String entity);
-    List<T> readAll();
+    List<T> getAll();
     void update(T entity);
     void delete(T entity);
 }
