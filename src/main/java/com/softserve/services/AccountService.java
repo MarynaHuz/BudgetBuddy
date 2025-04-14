f5be49749722c18e3a3dc24711d51114ef460636
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-14 11:18:46 +0300

    Add account management methods to AccountService
    Introduced CRUD operations in AccountService using JsonAccountDAO. This enables saving, retrieving, updating, and deleting Account objects, improving overall functionality.

--- src/main/java/com/softserve/services/AccountService.java
+++ src/main/java/com/softserve/services/AccountService.java
@@ -1,4 +1,33 @@
 package com.softserve.services;
 
+import com.softserve.dao.DAO;
+import com.softserve.dao.impl.JsonAccountDAO;
+import com.softserve.models.account.Account;
+
+import java.util.List;
+import java.util.Optional;
+
 public class AccountService {
+
+    private final DAO<Account> dao = new JsonAccountDAO();
+
+    public void addAccount(Account account) {
+        dao.save(account);
+    }
+
+    public Optional<Account> findAccountByName(String name) {
+        return dao.read(name);
+    }
+
+    public List<Account> findAllAccounts() {
+        return dao.readAll();
+    }
+
+    public void updateAccount(Account account) {
+        dao.update(account);
+    }
+
+    public void removeAccount(Account account) {
+        dao.delete(account);
+    }
 }
