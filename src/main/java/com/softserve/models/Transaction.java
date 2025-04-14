53e836b624460ea89eae34d85d4276a5e368c0f0
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-14 20:38:56 +0300

    Update Transaction model with new fields and validation logic
    Replaced `transactionId` type with `long` and introduced `BigDecimal` for `transactionAmount` to enhance precision. Added a constructor, validation for the amount, and default description logic based on transaction type. These changes improve data integrity and flexibility.

--- src/main/java/com/softserve/models/Transaction.java
+++ src/main/java/com/softserve/models/Transaction.java
@@ -5,20 +5,36 @@ import com.softserve.models.category.Category;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import static com.softserve.validators.AmountValidator.validateAmount;
 
 @NoArgsConstructor
 @EqualsAndHashCode
 public class Transaction {
 
-    private String transactionId;
-
+    private long transactionId;
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate transactionDate;
-    private Category category;
-    private double transactionAmount;
+    private Category transactionType;
+    private BigDecimal transactionAmount;
     private String description;
 
-    private static int idCounter = 100;
+    private static int nextId = 100;
 
+    public Transaction(LocalDate transactionDate,
+                       Category transactionType,
+                       String transactionAmount,
+                       String description) {
+
+        this.transactionId = nextId++;
+        this.transactionDate = transactionDate;
+        this.transactionType = transactionType;
+        this.transactionAmount = validateAmount(transactionAmount,
+                transactionType.getCategoryType());
+        this.description =
+                description != null ? description : transactionType.getCategoryName();
+
+    }
 }
