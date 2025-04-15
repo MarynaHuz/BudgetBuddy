bb0ab00f35f94b77c3bca301d31a39cd946638bf
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-15 12:11:02 +0300

    Add regex constant for account name validation

--- src/main/java/com/softserve/validators/AccountNameValidator.java
+++ src/main/java/com/softserve/validators/AccountNameValidator.java
@@ -4,6 +4,11 @@ public class AccountNameValidator {
 
     private AccountNameValidator() {
     }
+    private static final String NAME_MATCH_REGEX = """
+            ^[a-zA-Z]
+            (?=.*\\b[a-zA-Z0-9]{3,}\\b)
+            [a-zA-Z0-9 ]+$
+            """;
 
 
 }
