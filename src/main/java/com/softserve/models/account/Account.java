dc057b6f81c0f43a2a4b04d934e45da643aabf00
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-15 14:28:38 +0300

    Refactor Account model to use balance with validation
    Replaced `initialBalance` with `balance` and added `@Positive` annotation to enforce positivity. Updated import statements and related methods to align with the changes. This ensures better clarity and consistency in balance handling.

--- src/main/java/com/softserve/models/account/Account.java
+++ src/main/java/com/softserve/models/account/Account.java
@@ -1,14 +1,15 @@
 package com.softserve.models.account;
 
 import com.softserve.models.Transaction;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.softserve.validators.AccountValidation.validateCurrency;
 import static com.softserve.validators.BalanceValidator.validateBalance;
+import static com.softserve.validators.CurrencyValidator.validateCurrency;
 
 @NoArgsConstructor
 @Getter
@@ -22,14 +23,15 @@ public class Account {
     @Setter
     private String accountName;
     private Currency currency;
-    private BigDecimal initialBalance;
+    @Positive
+    private BigDecimal balance;
     private List<Transaction> transactions;
 
-    public Account(String accountName, String currency, String initialBalance) {
+    public Account(String accountName, String currency, String balance) {
         this.id = nextId++;
         this.accountName = accountName;
         this.currency = validateCurrency(currency);
-        this.initialBalance = validateBalance(initialBalance);
+        this.balance = validateBalance(balance);
         this.transactions = new ArrayList<>();
     }
 }
