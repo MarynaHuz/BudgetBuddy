24203402af08e6d1ce6971be98835e57a6b1d7e7
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-16 21:46:55 +0300

    Refactor TransactionService to implement Service interface
    Introduced the Service interface to TransactionService and added method stubs for CRUD operations. Initialized DAO with JsonTransactionDAO for further development.

--- src/main/java/com/softserve/services/TransactionService.java
+++ src/main/java/com/softserve/services/TransactionService.java
@@ -1,4 +1,40 @@
 package com.softserve.services;
 
-public class TransactionService {
+import com.softserve.dao.DAO;
+import com.softserve.dao.impl.JsonTransactionDAO;
+import com.softserve.models.transaction.Transaction;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TransactionService implements Service<Transaction> {
+
+    private final DAO<Transaction> dao = new JsonTransactionDAO();
+
+    //TODO: implement methods
+
+    @Override
+    public void create(Transaction transaction) {
+
+    }
+
+    @Override
+    public Optional<Transaction> findById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Transaction> listAll() {
+        return List.of();
+    }
+
+    @Override
+    public void update(Transaction entity) {
+
+    }
+
+    @Override
+    public void removeById(String id) {
+
+    }
 }
