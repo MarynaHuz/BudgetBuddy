7c34935f9f3797c6f0f92898d0f9b2d868ccd8bd
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-16 21:23:54 +0300

    Add CRUD method definitions to Service interface
    Introduced standard CRUD operations: save, read, getAll, update, and delete in the Service interface. This enhances usability and ensures consistency across service implementations.

--- src/main/java/com/softserve/services/Service.java
+++ src/main/java/com/softserve/services/Service.java
@@ -1,4 +1,13 @@
 package com.softserve.services;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface Service <T>{
+
+    void save(T entity);
+    Optional<T> read(String id);
+    List<T> getAll();
+    void update(T entity);
+    void delete(String id);
 }
