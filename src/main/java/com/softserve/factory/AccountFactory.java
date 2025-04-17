a2fa1d2a785923e98d6032334d89065078ff6b58
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-17 15:05:10 +0300

    Add `createAccount` method to `AccountFactory`
    Introduced a static `createAccount` method to construct `Account` objects. It includes validation for account name, currency, and balance, and generates a unique ID for the account. This improves the creation process by enforcing consistent validation and encapsulating logic within the factory.

--- src/main/java/com/softserve/factory/AccountFactory.java
+++ src/main/java/com/softserve/factory/AccountFactory.java
@@ -1,4 +1,34 @@
 package com.softserve.factory;
 
+import com.softserve.models.account.Account;
+import com.softserve.models.account.Currency;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static com.softserve.utils.IdGeneratorUtil.generateNextId;
+import static com.softserve.validators.AccountNameValidator.validateAccountName;
+import static com.softserve.validators.BalanceValidator.validateBalance;
+import static com.softserve.validators.CurrencyValidator.validateCurrency;
+
 public class AccountFactory {
+
+    private AccountFactory() {
+    }
+
+    public static Account createAccount(String accountName, String currency,
+                                        String balance, List<Account> accounts) {
+        String validatedName = validateAccountName(accountName);
+        Currency validatedCurrency = validateCurrency(currency);
+        BigDecimal validatedBalance = validateBalance(balance);
+
+        int nextId = generateNextId(accounts, Account::getId);
+
+        return Account.builder()
+                .id(nextId)
+                .accountName(validatedName)
+                .currency(validatedCurrency)
+                .balance(validatedBalance)
+                .build();
+    }
 }
