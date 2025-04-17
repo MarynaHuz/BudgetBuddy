2e8bb6c6a2d62831368a54994ae8be1bc88e30e5
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-17 20:35:04 +0300

    Add utility to ensure parent directory existence
    Introduce `ensureDirectoryExists` method in `FileUtils` to verify and create parent directories if they are missing. This improves file handling robustness by preventing issues with non-existent directories during file operations.

--- src/main/java/com/softserve/utils/FileUtils.java
+++ src/main/java/com/softserve/utils/FileUtils.java
@@ -1,6 +1,7 @@
 package com.softserve.utils;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileUtils {
 
@@ -11,5 +12,18 @@ public class FileUtils {
         File file = new File(filePath);
         return file.exists() && file.isFile();
     }
+
+    public static void ensureDirectoryExists(String filePath) throws IOException {
+        File file = new File(filePath);
+        File parentDir = file.getParentFile();
+
+        if (parentDir != null && !parentDir.exists()) {
+            boolean created = parentDir.mkdirs();
+            if (!created) {
+                throw new IOException("Failed to create directory: " + parentDir.getAbsolutePath());
+            }
+        }
+    }
+
 
 }
