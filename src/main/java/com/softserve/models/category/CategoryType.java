735551e3f574252d6fb1aaf02e3fb2607f542733
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-18 11:02:06 +0300

    Simplify CategoryType enum by removing unused code.
    Removed redundant fields, annotations, and methods from the CategoryType enum. This change improves code clarity and eliminates unnecessary complexity.

--- src/main/java/com/softserve/models/category/CategoryType.java
+++ src/main/java/com/softserve/models/category/CategoryType.java
@@ -1,26 +1,7 @@
 package com.softserve.models.category;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-import java.math.BigDecimal;
-
-@Getter
-@AllArgsConstructor
 public enum CategoryType {
 
-    INCOME("Income"),
-    EXPENSE("Expense");
-
-    private final String categoryTypeName;
-
-    public static boolean isValidAmountForCategory(BigDecimal amount,
-                                                   CategoryType categoryType) {
-        if (categoryType.equals(INCOME)) {
-            return amount.compareTo(BigDecimal.ZERO) > 0;
-        } else if (categoryType.equals(EXPENSE)) {
-            return amount.compareTo(BigDecimal.ZERO) <= 0;
-        }
-        return false;
-    }
+    INCOME,
+    EXPENSE
 }
