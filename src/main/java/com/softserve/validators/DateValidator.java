32e7bbaa357cc5c446f05b1aee44396234a1e56c
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-18 15:33:27 +0300

    Validate that dates are not set in the future.

--- src/main/java/com/softserve/validators/DateValidator.java
+++ src/main/java/com/softserve/validators/DateValidator.java
@@ -12,20 +12,25 @@ public class DateValidator {
             "Invalid date: date cannot be null or blank.";
     private static final String FORMAT_ERROR_MESSAGE =
             "Invalid date format: expected format is " + DATE_FORMAT + ".";
+    private static final String FUTURE_DATE_ERROR_MESSAGE =
+            "Invalid date: date cannot be in the future.";
+
 
     private DateValidator() {
     }
 
     public static LocalDate validateDate(String date) {
         validateDateNotNullOrBlank(date);
-        return parseDate(date.trim());
+        LocalDate parsedDate = parseDate(date.trim());
+        return validateNotFutureDate(parsedDate);
     }
 
     private static LocalDate parseDate(String date) {
         try {
             return LocalDate.parse(date, FORMATTER);
         } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException(FORMAT_ERROR_MESSAGE + " Provided value: " + date, e);
+            throw new IllegalArgumentException(FORMAT_ERROR_MESSAGE +
+                    " Provided value: " + date, e);
         }
     }
 
@@ -33,5 +38,14 @@ public class DateValidator {
         if (date == null || date.trim().isEmpty()) {
             throw new IllegalArgumentException(NULL_OR_BLANK_DATE_MESSAGE);
         }
+    }
+
+    private static LocalDate validateNotFutureDate(LocalDate date) {
+
+        if (date.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException(FUTURE_DATE_ERROR_MESSAGE +
+                    " Provided value: " + date);
+        }
+        return date;
     }
 }
