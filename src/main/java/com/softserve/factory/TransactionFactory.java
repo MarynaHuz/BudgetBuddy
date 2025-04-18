a61e4a3f9cefd01b5a000bca64c163b0cb6d9ce7
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-18 12:02:29 +0300

    Add createTransaction method to TransactionFactory
    Introduce a static method to create and validate transactions, including fields like accountId, transactionDate, transactionType, and transactionAmount. Add validations for input values and generate a unique transaction ID. Placeholder logic for accountId validation and currency assignment is noted for future implementation.

--- src/main/java/com/softserve/factory/TransactionFactory.java
+++ src/main/java/com/softserve/factory/TransactionFactory.java
@@ -1,9 +1,46 @@
 package com.softserve.factory;
 
+import com.softserve.models.account.Currency;
+import com.softserve.models.category.Category;
+import com.softserve.models.transaction.Transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.softserve.utils.IdGeneratorUtil.generateNextId;
+import static com.softserve.validators.AmountValidator.validateAmount;
+import static com.softserve.validators.DateValidator.validateDate;
+import static com.softserve.validators.EnumValidator.validateEnum;
+
 public class TransactionFactory {
 
     private TransactionFactory() {
     }
 
+    public static <E extends Enum<E> & Category> Transaction createTransaction(int accountId,
+                                                                               String transactionType,
+                                                                               String transactionDate,
+                                                                               String transactionAmount,
+                                                                               Class<E> enumClass,
+                                                                               List<Transaction> transactions) {
+        //TODO: add accountId validator
+        int validatedAccountId = accountId;
+        LocalDate validatedTransactionDate = validateDate(transactionDate);
+        Category validatedTransactionType = validateEnum(transactionType, enumClass);
+        BigDecimal validatedTransactionAmount =
+                validateAmount(transactionAmount, validatedTransactionType);
+        Currency validatedCurrency = Currency.UAH; //TODO: add logic to get currency by accountId
 
+        int nextId = generateNextId(transactions, Transaction::getTransactionId);
+
+        return Transaction.builder()
+                .accountId(validatedAccountId)
+                .transactionId(nextId)
+                .transactionDate(validatedTransactionDate)
+                .transactionType(validatedTransactionType)
+                .transactionAmount(validatedTransactionAmount)
+                .currency(validatedCurrency)
+                .build();
+    }
 }
