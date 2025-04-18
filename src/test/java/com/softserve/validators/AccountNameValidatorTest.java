5de0be5d7e5f586e5edb502249378883dcead0d3
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-18 15:18:36 +0300

    Refactor test imports for AccountNameValidator methods

--- src/test/java/com/softserve/validators/AccountNameValidatorTest.java
+++ src/test/java/com/softserve/validators/AccountNameValidatorTest.java
@@ -2,47 +2,49 @@ package com.softserve.validators;
 
 import org.junit.jupiter.api.Test;
 
+import static com.softserve.validators.AccountNameValidator.isValid;
+import static com.softserve.validators.AccountNameValidator.validateAccountName;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountNameValidatorTest {
 
     @Test
     void validateAccountName_ReturnsName_IfNameIsValid() {
-        assertEquals("Savings USD", AccountNameValidator.validateAccountName("Savings USD"));
-        assertEquals("Privatbank", AccountNameValidator.validateAccountName("Privatbank"));
+        assertEquals("Savings USD", validateAccountName("Savings USD"));
+        assertEquals("Privatbank", validateAccountName("Privatbank"));
     }
 
     @Test
     void validateAccountName_ThrowsException_IfNameIsInvalid() {
-        assertThrows(IllegalArgumentException.class, () -> AccountNameValidator.validateAccountName("1InvalidName"));
-        assertThrows(IllegalArgumentException.class, () -> AccountNameValidator.validateAccountName("J"));
-        assertThrows(IllegalArgumentException.class, () -> AccountNameValidator.validateAccountName("!*&#"));
+        assertThrows(IllegalArgumentException.class, () -> validateAccountName("1InvalidName"));
+        assertThrows(IllegalArgumentException.class, () -> validateAccountName("J"));
+        assertThrows(IllegalArgumentException.class, () -> validateAccountName("!*&#"));
     }
 
     @Test
     void isValid_ReturnsTrue_IfNameIsValid() {
-        assertTrue(AccountNameValidator.isValid("Savings USD"));
-        assertTrue(AccountNameValidator.isValid("Monobank"));
+        assertTrue(isValid("Savings USD"));
+        assertTrue(isValid("Monobank"));
     }
 
     @Test
     void isValid_ReturnsFalse_IfNameIsInvalid() {
-        assertFalse(AccountNameValidator.isValid("1InvalidName"));
-        assertFalse(AccountNameValidator.isValid("J"));
-        assertFalse(AccountNameValidator.isValid("!*&#"));
+        assertFalse(isValid("1InvalidName"));
+        assertFalse(isValid("J"));
+        assertFalse(isValid("!*&#"));
     }
 
     @Test
     void isValid_ReturnsFalse_IfNameIsNullOrBlank() {
-        assertFalse(AccountNameValidator.isValid(null));
-        assertFalse(AccountNameValidator.isValid(""));
-        assertFalse(AccountNameValidator.isValid("  "));
+        assertFalse(isValid(null));
+        assertFalse(isValid(""));
+        assertFalse(isValid("  "));
     }
 
     @Test
     void validateAccountName_ThrowsException_IfNameIsNullOrBlank() {
-        assertThrows(IllegalArgumentException.class, () -> AccountNameValidator.validateAccountName(null));
-        assertThrows(IllegalArgumentException.class, () -> AccountNameValidator.validateAccountName(""));
-        assertThrows(IllegalArgumentException.class, () -> AccountNameValidator.validateAccountName("  "));
+        assertThrows(IllegalArgumentException.class, () -> validateAccountName(null));
+        assertThrows(IllegalArgumentException.class, () -> validateAccountName(""));
+        assertThrows(IllegalArgumentException.class, () -> validateAccountName("  "));
     }
 }
