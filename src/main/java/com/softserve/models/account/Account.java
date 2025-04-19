30f933844d6bf2b1eb3521dda3a4b89063310e59
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-19 21:37:09 +0300

    Add validation for account name and restrict currency changes
    Introduced a `setAccountName` method that validates names using `AccountNameValidator`. Added a placeholder in `setCurrency` to prevent currency changes if transactions exist. This enhances data consistency and enforces stricter rules.

--- src/main/java/com/softserve/models/account/Account.java
+++ src/main/java/com/softserve/models/account/Account.java
@@ -5,6 +5,8 @@ import lombok.*;
 
 import java.math.BigDecimal;
 
+import static com.softserve.validators.AccountNameValidator.validateAccountName;
+
 
 @ToString
 @EqualsAndHashCode
@@ -14,10 +16,20 @@ import java.math.BigDecimal;
 @Builder
 public class Account {
 
-    private int accountId;
     @Setter
+    private int accountId;
     private String accountName;
     private Currency currency;
     @Positive
     private BigDecimal balance;
+
+    public void setAccountName(String accountName) {
+        this.accountName = validateAccountName(accountName);
+    }
+
+    public void setCurrency(Currency currency) {
+        //TODO: check if any transactions were added to this account.
+        // If they were added you can't change the currency due to problems with exchange rate
+        this.currency = currency;
+    }
 }
