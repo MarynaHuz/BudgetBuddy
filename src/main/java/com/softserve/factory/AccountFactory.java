366d061601653ab5dc136fd2b06481ef256a25fc
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-19 17:26:04 +0300

    Refactor AccountFactory to simplify account creation and centralize ID generation.
    Removed unnecessary dependencies on the account list and ID generator from the AccountFactory. Replaced dynamic ID generation with a default placeholder value to streamline the `createAccount` method. The responsibility for generating unique account IDs has been moved to the service layer to ensure that IDs are assigned only after account fields have been validated, maintaining data integrity.

--- src/main/java/com/softserve/factory/AccountFactory.java
+++ src/main/java/com/softserve/factory/AccountFactory.java
@@ -4,9 +4,7 @@ import com.softserve.models.account.Account;
 import com.softserve.models.account.Currency;
 
 import java.math.BigDecimal;
-import java.util.List;
 
-import static com.softserve.utils.IdGeneratorUtil.generateNextId;
 import static com.softserve.validators.AccountNameValidator.validateAccountName;
 import static com.softserve.validators.BalanceValidator.validateBalance;
 import static com.softserve.validators.EnumValidator.validateEnum;
@@ -17,15 +15,13 @@ public class AccountFactory {
     }
 
     public static Account createAccount(String accountName, String currency,
-                                        String balance, List<Account> accounts) {
+                                        String balance) {
         String validatedName = validateAccountName(accountName);
         Currency validatedCurrency = validateEnum(currency, Currency.class);
         BigDecimal validatedBalance = validateBalance(balance);
 
-        int nextId = generateNextId(accounts, Account::getAccountId);
-
         return Account.builder()
-                .accountId(nextId)
+                .accountId(0)
                 .accountName(validatedName)
                 .currency(validatedCurrency)
                 .balance(validatedBalance)
