bf22f7b695b123ce3e8fbd4005685cbdad8ace89
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-19 15:11:22 +0300

    Refactor JsonAccountDAO to handle JSON file operations
    Replaced hardcoded data with JSON file reading logic for account retrieval. Added error handling for missing or inaccessible JSON files using NoSuchFileException and UncheckedIOException. Improved resilience and maintainability of account data access.

--- src/main/java/com/softserve/dao/impl/JsonAccountDAO.java
+++ src/main/java/com/softserve/dao/impl/JsonAccountDAO.java
@@ -1,10 +1,18 @@
 package com.softserve.dao.impl;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.softserve.dao.DAO;
 import com.softserve.models.account.Account;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.NoSuchFileException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static com.softserve.utils.AppConfig.ACCOUNTS_JSON;
+import static com.softserve.utils.JsonUtil.readFromJson;
 
 public class JsonAccountDAO implements DAO<Account> {
 
@@ -15,16 +23,20 @@ public class JsonAccountDAO implements DAO<Account> {
 
     @Override
     public Optional<Account> read(String accountId) {
-
-        return getAll().stream()
-                .filter(acc ->
-                        acc.getAccountName().equals(accountId))
-                .findFirst();
+        return Optional.empty();
     }
 
     @Override
     public List<Account> getAll() {
-        return List.of();
+        try {
+            return readFromJson(ACCOUNTS_JSON.getPath(), new TypeReference<>() {});
+        } catch (NoSuchFileException e){
+            System.err.println("Account file not found: " + ACCOUNTS_JSON.getPath());
+            return new ArrayList<>();
+        } catch (IOException e) {
+            throw new UncheckedIOException(
+                    "Failed to retrieve all accounts from JSON file", e);
+        }
     }
 
     @Override
@@ -34,5 +46,6 @@ public class JsonAccountDAO implements DAO<Account> {
 
     @Override
     public void delete(Account account) {
+
     }
 }
