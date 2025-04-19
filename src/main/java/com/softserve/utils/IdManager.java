da7bc6b42d31611c5c829e8a58ce2c9901513bef
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-19 14:23:04 +0300

    Add IdManager utility class for managing IDs

---
+++ src/main/java/com/softserve/utils/IdManager.java
@@ -0,0 +1,23 @@
+package com.softserve.utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.IOException;
+
+import static com.softserve.utils.JsonUtil.readFromJson;
+
+public class IdManager {
+
+    private IdManager() {
+    }
+
+
+    private static int getLastId(String filePath) {
+        try {
+            Integer lastId = readFromJson(filePath, new TypeReference<>() {});
+            return lastId != null? lastId: 0;
+        } catch (IOException e) {
+            return 0;
+        }
+    }
+}
