2b288a19b4dfad1bf828ad40a71da94743f3acc8
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-19 23:18:01 +0300

    Refactor TransactionFactory to enhance validation logic
    Replaced integer accountId with String and integrated account ID validation using `validateId`. Removed reliance on external `transactions` for ID generation. Simplified unused imports and updated comments to improve clarity.

--- src/main/java/com/softserve/factory/TransactionFactory.java
+++ src/main/java/com/softserve/factory/TransactionFactory.java
@@ -1,46 +1,45 @@
 package com.softserve.factory;
 
+import com.softserve.models.account.Account;
 import com.softserve.models.account.Currency;
 import com.softserve.models.category.Category;
 import com.softserve.models.transaction.Transaction;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
 
-import static com.softserve.utils.IdGeneratorUtil.generateNextId;
+import static com.softserve.utils.AppConfig.ACCOUNTS_JSON;
 import static com.softserve.validators.AmountValidator.validateAmount;
 import static com.softserve.validators.DateValidator.validateDate;
 import static com.softserve.validators.EnumValidator.validateEnum;
+import static com.softserve.validators.IdValidator.validateId;
 
 public class TransactionFactory {
 
     private TransactionFactory() {
     }
 
-    public static <E extends Enum<E> & Category> Transaction createTransaction(int accountId,
+    public static <E extends Enum<E> & Category> Transaction createTransaction(String accountId,
                                                                                String transactionType,
                                                                                String transactionDate,
                                                                                String transactionAmount,
-                                                                               Class<E> enumClass,
-                                                                               List<Transaction> transactions) {
-        //TODO: add accountId validator
-        int validatedAccountId = accountId;
+                                                                               Class<E> enumClass) throws IOException {
+        int validatedAccountId = validateId(ACCOUNTS_JSON.getPath(),accountId,
+                Account::getAccountId
+        );
         LocalDate validatedTransactionDate = validateDate(transactionDate);
         Category validatedTransactionType = validateEnum(transactionType, enumClass);
         BigDecimal validatedTransactionAmount =
                 validateAmount(transactionAmount, validatedTransactionType);
-        Currency validatedCurrency = Currency.UAH; //TODO: add logic to get currency by accountId
-
-        int nextId = generateNextId(transactions, Transaction::getTransactionId);
 
         return Transaction.builder()
                 .accountId(validatedAccountId)
-                .transactionId(nextId)
+                .transactionId(0)
                 .transactionDate(validatedTransactionDate)
                 .transactionType(validatedTransactionType)
                 .transactionAmount(validatedTransactionAmount)
-                .currency(validatedCurrency)
+                .currency(Currency.UAH)
                 .build();
     }
 }
