7c5fad08bb1a666c0937a75d6aab29695b016db1
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-19 23:12:19 +0300

    Add validateId method to IdValidator
    Introduced a new `validateId` method to validate and parse IDs from a JSON file while ensuring they match existing items using a provided ID extractor function. This enhances reusability and validation logic within the application. Also included necessary imports for JSON handling and functional programming.

--- src/main/java/com/softserve/validators/IdValidator.java
+++ src/main/java/com/softserve/validators/IdValidator.java
@@ -1,5 +1,14 @@
 package com.softserve.validators;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.ToIntFunction;
+
+import static com.softserve.utils.JsonUtil.readFromJson;
+
 public class IdValidator {
 
     private IdValidator() {
@@ -7,6 +16,32 @@ public class IdValidator {
 
     private static final String PARSING_ERROR_MESSAGE =
             "ID must be a positive number";
+
+
+    public static <T> int validateId(String filePath, String id,
+                                     ToIntFunction<T> idExtractor) throws IOException {
+        int parsedId = parseId(id);
+
+        if (isPositiveNumber(parsedId)) {
+
+            List<T> items = new ArrayList<>(
+                    readFromJson(filePath, new TypeReference<>() {
+                    }));
+
+            try {
+                for (var item : items) {
+
+                    Integer itemId = idExtractor.applyAsInt(item);
+                    if (itemId.equals(parsedId)) {
+                        return parsedId;
+                    }
+                }
+            } catch (Exception e) {
+                return 0;
+            }
+        }
+        return 0;
+    }
 
     private static int parseId(String id) {
         if (id == null || id.trim().isEmpty() || !isNumber(id.trim())) {
