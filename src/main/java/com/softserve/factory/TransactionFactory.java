4a098889a54844da4fe8d8304789f8e65080a05e
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-20 18:16:09 +0300

    Refactor TransactionFactory to simplify method signature.
    Removed the generic enum parameter from the `createTransaction` method and replaced `validateEnum` with `getCategoryByName` for transaction type validation. This enhances clarity by reducing complexity and streamlines category handling logic.

--- src/main/java/com/softserve/factory/TransactionFactory.java
+++ src/main/java/com/softserve/factory/TransactionFactory.java
@@ -10,9 +10,9 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static com.softserve.utils.AppConfig.ACCOUNTS_JSON;
+import static com.softserve.utils.CategoryManager.getCategoryByName;
 import static com.softserve.validators.AmountValidator.validateAmount;
 import static com.softserve.validators.DateValidator.validateDate;
-import static com.softserve.validators.EnumValidator.validateEnum;
 import static com.softserve.validators.IdValidator.validateId;
 
 public class TransactionFactory {
@@ -20,17 +20,17 @@ public class TransactionFactory {
     private TransactionFactory() {
     }
 
-    public static <E extends Enum<E> & Category> Transaction createTransaction(String accountId,
-                                                                               String transactionType,
-                                                                               String transactionDate,
-                                                                               String transactionAmount,
-                                                                               Class<E> enumClass) throws IOException {
+    public static Transaction createTransaction(String accountId,
+                                                String transactionType,
+                                                String transactionDate,
+                                                String transactionAmount
+    ) throws IOException {
         int validatedAccountId = validateId(ACCOUNTS_JSON.getPath(), accountId,
                 Account::getAccountId);
         LocalDate validatedTransactionDate = validateDate(transactionDate);
-        Category validatedTransactionType = validateEnum(transactionType, enumClass);
-        BigDecimal validatedTransactionAmount =
-                validateAmount(transactionAmount, validatedTransactionType);
+        Category validatedTransactionType = getCategoryByName(transactionType);
+        BigDecimal validatedTransactionAmount = validateAmount(transactionAmount,
+                validatedTransactionType);
 
         return Transaction.builder()
                 .accountId(validatedAccountId)
