9bc1ee63c2617e946bbc6e9dc2a936770952aec8
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-20 19:55:35 +0300

    Add skeleton implementation for TransactionController
    Introduced TransactionController with unimplemented CRUD methods. This also includes a private TransactionService instance for future service logic.

--- src/main/java/com/softserve/controllers/TransactionController.java
+++ src/main/java/com/softserve/controllers/TransactionController.java
@@ -1,6 +1,37 @@
 package com.softserve.controllers;
 
 import com.softserve.models.transaction.Transaction;
+import com.softserve.services.Service;
+import com.softserve.services.TransactionService;
 
-public class TransactionController implements Controller<Transaction>{
+public class TransactionController implements Controller<Transaction> {
+
+    private final Service transactionService = new TransactionService();
+
+    //TODO: Implement methods
+
+    @Override
+    public void create(Transaction entity) {
+
+    }
+
+    @Override
+    public void findById(String id) {
+
+    }
+
+    @Override
+    public void listAll() {
+
+    }
+
+    @Override
+    public void update(Transaction entity) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
 }
