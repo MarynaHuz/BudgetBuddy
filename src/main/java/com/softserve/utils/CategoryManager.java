edbd045a54b80b2837910f18779c4ef75a430861
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-20 15:36:47 +0300

    Add CATEGORY_LOOKUP map to CategoryManager
    Initialized a static CATEGORY_LOOKUP map using ExpenseCategory and IncomeCategory enums. This enables efficient lookup of categories by their uppercase names for better performance and streamlined accessibility.

--- src/main/java/com/softserve/utils/CategoryManager.java
+++ src/main/java/com/softserve/utils/CategoryManager.java
@@ -1,9 +1,28 @@
 package com.softserve.utils;
 
+import com.softserve.models.category.Category;
+import com.softserve.models.category.ExpenseCategory;
+import com.softserve.models.category.IncomeCategory;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CategoryManager {
 
     private CategoryManager() {
     }
 
+    private static final Map<String, Category> CATEGORY_LOOKUP;
 
+    static {
+        CATEGORY_LOOKUP = Stream.concat(
+                Arrays.stream(ExpenseCategory.values()),
+                Arrays.stream(IncomeCategory.values())
+        ).collect(Collectors.toMap(
+                c -> c.getCategoryName().toUpperCase(),
+                Function.identity()));
+    }
 }
