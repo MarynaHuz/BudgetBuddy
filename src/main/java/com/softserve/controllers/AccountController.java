1e0caa91eae470b603e2d1f67d4f33415d5601ab
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-20 19:54:53 +0300

    Add initial skeleton for AccountController implementation
    Introduced AccountController implementing the Controller interface for Account entities. Added method stubs for CRUD operations and initialized AccountService for future use. Further implementation is required to complete functionality.

--- src/main/java/com/softserve/controllers/AccountController.java
+++ src/main/java/com/softserve/controllers/AccountController.java
@@ -1,4 +1,37 @@
 package com.softserve.controllers;
 
-public class AccountController {
+import com.softserve.models.account.Account;
+import com.softserve.services.AccountService;
+import com.softserve.services.Service;
+
+public class AccountController implements Controller<Account> {
+
+    private final Service accountService = new AccountService();
+
+    //TODO: Implement methods
+
+    @Override
+    public void create(Account entity) {
+
+    }
+
+    @Override
+    public void findById(String id) {
+
+    }
+
+    @Override
+    public void listAll() {
+
+    }
+
+    @Override
+    public void update(Account entity) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
 }
