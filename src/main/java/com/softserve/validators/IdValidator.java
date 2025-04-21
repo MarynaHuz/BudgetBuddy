6e7eb8da259f2656431c80733312f491763f66be
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-21 13:05:54 +0300

    Simplify IdValidator and refactor ID validation logic.
    Removed unused imports and eliminated redundant parameters from the `validateId` method. Replaced `isPositiveNumber` check with a `validatePositive` method to throw exceptions for invalid IDs.

--- src/main/java/com/softserve/validators/IdValidator.java
+++ src/main/java/com/softserve/validators/IdValidator.java
@@ -1,14 +1,6 @@
 package com.softserve.validators;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.function.ToIntFunction;
-
-import static com.softserve.utils.JsonUtil.readFromJson;
 
 public class IdValidator {
 
@@ -18,26 +10,10 @@ public class IdValidator {
     private static final String PARSING_ERROR_MESSAGE =
             "ID must be a positive number";
 
-
-    public static <T> int validateId(String filePath, String id,
-                                     ToIntFunction<T> idExtractor) throws IOException {
+    public static int validateId(String id) {
         int parsedId = parseId(id);
-
-        if (!isPositiveNumber(parsedId)) {
-            return 0;
-        }
-
-        List<T> items = readFromJson(filePath, new TypeReference<List<T>>() {
-        })
-                .orElseGet(ArrayList::new);
-
-        for (var item : items) {
-            Integer itemId = idExtractor.applyAsInt(item);
-            if (itemId.equals(parsedId)) {
-                return parsedId;
-            }
-        }
-        return 0;
+        validatePositive(parsedId);
+        return parsedId;
     }
 
     private static int parseId(String id) {
@@ -49,11 +25,9 @@ public class IdValidator {
         );
     }
 
-    private static boolean isPositiveNumber(int id) {
+    private static void validatePositive(int id) {
         if (id <= 0) {
-            System.err.println(PARSING_ERROR_MESSAGE);
-            return false;
+            throw new IllegalArgumentException(PARSING_ERROR_MESSAGE);
         }
-        return true;
     }
 }
