402af13ca22686a831dbce9cbf700e69b9334826
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-21 11:49:29 +0300

    Refactor JsonUtil: remove addToJson method

--- src/main/java/com/softserve/utils/JsonUtil.java
+++ src/main/java/com/softserve/utils/JsonUtil.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static com.softserve.utils.FileUtils.ensureFileExists;
@@ -34,8 +32,8 @@ public class JsonUtil {
     }
 
     public static <T> Optional<T> readFromJson(String filePath, TypeReference<T> typeRef) throws IOException {
-
         validateFilePath(filePath);
+
         File file = new File(filePath);
 
         if (!file.exists()) {
@@ -47,25 +45,12 @@ public class JsonUtil {
         return Optional.of(OBJECT_MAPPER.readValue(file, typeRef));
     }
 
-    public static <T> void addToJson(String filePath, T itemToAdd,
-                                     TypeReference<List<T>> typeRef) throws IOException {
-
-        File file = ensureFileExists(filePath);
-        List<T> items = readFromJson(filePath, typeRef).orElseGet(ArrayList::new);
-        items.add(itemToAdd);
-        serializeToJson(file, items);
-    }
-
     public static <T> void writeToJson(String filePath, T data) throws IOException {
         File file = ensureFileExists(filePath);
-        serializeToJson(file, data);
-    }
 
-    private static <T> void serializeToJson(File file, T data) throws IOException {
         try (var outputStream = new FileOutputStream(file)) {
             OBJECT_MAPPER.writerWithDefaultPrettyPrinter()
                     .writeValue(outputStream, data);
         }
     }
-
 }
