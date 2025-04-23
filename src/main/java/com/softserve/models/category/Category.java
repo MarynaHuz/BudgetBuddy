03289364181c944efc5bb72365390f67028dd817
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-23 15:05:44 +0300

    Remove isValidAmount method from Category interface
    The isValidAmount method was removed as it was deemed unnecessary for the Category interface. This simplifies the interface, ensuring it focuses only on essential methods related to category details.

--- src/main/java/com/softserve/models/category/Category.java
+++ src/main/java/com/softserve/models/category/Category.java
@@ -2,11 +2,9 @@ package com.softserve.models.category;
 
 import com.softserve.models.transaction.TransactionType;
 
-import java.math.BigDecimal;
-
 public interface Category {
 
     String getCategoryName();
-    boolean isValidAmount(BigDecimal amount);
     TransactionType getCategoryType();
+
 }
