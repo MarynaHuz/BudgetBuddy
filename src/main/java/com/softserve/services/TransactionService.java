8f866062b7e47449ae5f8d6fdfb17e65b77e8b62
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-23 17:54:16 +0300

    Refactor account existence check in TransactionService.
    Replaced `existById` with `findById` for account verification to ensure reliable fetching of account data. Adjusted transaction ID generation to occur only after confirming account existence. This improves code clarity and eliminates potential redundant operations.

--- src/main/java/com/softserve/services/TransactionService.java
+++ src/main/java/com/softserve/services/TransactionService.java
@@ -21,19 +21,20 @@ public class TransactionService implements Service<Transaction> {
 
     @Override
     public void create(Transaction transaction) throws IOException {
-        int generatedTransactionId = generateNextId(AppConfig.TRANSACTION_ID.getPath());
         int accountId = transaction.getAccountId();
 
-        if (accountService.existById(accountId)) {
+        if (accountService.findById(accountId).isPresent()) {
             transaction.setCurrency(accountService
                     .findById(accountId)
                     .get()
                     .getCurrency());
+
+            transaction.setTransactionId(
+                    generateNextId(AppConfig.TRANSACTION_ID.getPath()));
         } else {
             throw new IllegalArgumentException("Account with ID " +
                     transaction.getAccountId() + " does not exist");
         }
-        transaction.setTransactionId(generatedTransactionId);
         dao.save(transaction);
     }
 
