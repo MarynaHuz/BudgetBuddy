ed1110e03412b87b8e4cafd0abbd1d5b7ac77fab
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-23 16:08:02 +0300

    Add existsAccountById method to AccountService
    Introduced a new method to check if an account exists by its ID.

--- src/main/java/com/softserve/services/AccountService.java
+++ src/main/java/com/softserve/services/AccountService.java
@@ -57,4 +57,8 @@ public class AccountService implements Service<Account> {
     public void removeById(int id) {
 
     }
+
+    boolean existsAccountById(int id) throws IOException {
+        return (findById(id).isPresent());
+    }
 }
