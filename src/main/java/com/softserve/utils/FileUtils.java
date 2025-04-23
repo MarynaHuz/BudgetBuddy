9111008393bf3f49f4ea1786cb93426c09562999
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-23 13:58:59 +0300

    Refactor FileUtils to use java.nio.file APIs.
    Replaced usage of java.io.File with java.nio.file for better modern API usage and readability.

--- src/main/java/com/softserve/utils/FileUtils.java
+++ src/main/java/com/softserve/utils/FileUtils.java
@@ -1,45 +1,38 @@
 package com.softserve.utils;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileUtils {
 
     private FileUtils() {
     }
 
-    public static void ensureDirectoryExists(String filePath) throws IOException {
-        validateFilePath(filePath);
-        File file = new File(filePath);
-        File parentDir = file.getParentFile();
-
-        if (parentDir != null && !parentDir.exists()) {
-            boolean created = parentDir.mkdirs();
-            if (!created) {
-                throw new IOException("Failed to create directory: " +
-                        parentDir.getAbsolutePath());
-            }
-        }
-    }
-
-    public static File createFileIfNotExists(String filePath) throws IOException {
-        validateFilePath(filePath);
-        File file = new File(filePath);
-
-        if (!file.exists()) {
-            boolean created = file.createNewFile();
-            if (!created) {
-                throw new IOException("Failed to create file: " +
-                        file.getAbsolutePath());
-            }
-        }
-        return file;
-    }
-
-    public static File ensureFileExists(String filePath) throws IOException {
-        validateFilePath(filePath);
+    public static Path ensureFileExists(String filePath) throws IOException {
         ensureDirectoryExists(filePath);
         return createFileIfNotExists(filePath);
+    }
+
+    public static void ensureDirectoryExists(String filePath) throws IOException {
+        validateFilePath(filePath);
+        Path path = Paths.get(filePath);
+        Path parentDir = path.getParent();
+
+        if (parentDir != null && !Files.exists(parentDir)) {
+            Files.createDirectories(parentDir);
+        }
+    }
+
+    public static Path createFileIfNotExists(String filePath) throws IOException {
+        validateFilePath(filePath);
+        Path path = Paths.get(filePath);
+
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+        return path;
     }
 
     public static void validateFilePath(String filePath) {
