4260d534350ffc0696495efaa56466a90086cfbc
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-23 16:27:55 +0300

    Add ID generation and account validation in create method
    Enhanced the `create` method in `TransactionService` to generate a unique transaction ID and validate the existence of the associated account. Prevents creation of transactions for non-existent accounts and ensures proper ID assignment.

--- src/main/java/com/softserve/services/TransactionService.java
+++ src/main/java/com/softserve/services/TransactionService.java
@@ -2,20 +2,33 @@ package com.softserve.services;
 
 import com.softserve.dao.Dao;
 import com.softserve.dao.impl.JsonTransactionDao;
+import com.softserve.models.account.Account;
 import com.softserve.models.transaction.Transaction;
+import com.softserve.utils.AppConfig;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+
+import static com.softserve.utils.IdManager.generateNextId;
 
 public class TransactionService implements Service<Transaction> {
 
     private final Dao<Transaction> dao = new JsonTransactionDao();
+    private final Service<Account> accountService = new AccountService();
 
     //TODO: implement methods
 
     @Override
-    public void create(Transaction transaction) {
+    public void create(Transaction transaction) throws IOException {
+        int generatedId = generateNextId(AppConfig.TRANSACTION_ID.getPath());
 
+        if(!accountService.existById(transaction.getAccountId())){
+            throw new IllegalArgumentException("Account with ID " +
+                    transaction.getAccountId() + " does not exist");
+        }
+        transaction.setTransactionId(generatedId);
+        dao.save(transaction);
     }
 
     @Override
