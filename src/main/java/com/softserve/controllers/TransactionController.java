155147c5d6fbc350ec92e946c2b50d1c0dbb27be
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-24 22:21:17 +0300

    feat(transaction-controller): implement create method with validation
    Add input validation and exception handling for transaction creation. This includes validating ID, category, date, and amount, while leveraging a factory to create transaction objects. Exceptions are handled to ensure robust error management.

--- src/main/java/com/softserve/controllers/TransactionController.java
+++ src/main/java/com/softserve/controllers/TransactionController.java
@@ -1,10 +1,19 @@
 package com.softserve.controllers;
 
+import com.softserve.factory.TransactionFactory;
 import com.softserve.models.transaction.Transaction;
 import com.softserve.services.Service;
 import com.softserve.services.TransactionService;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+
+import static com.softserve.utils.CategoryManager.getCategoryByName;
+import static com.softserve.validators.AmountValidator.validateAmount;
+import static com.softserve.validators.DateValidator.validateDate;
+import static com.softserve.validators.IdValidator.validateId;
 
 public class TransactionController implements Controller<Transaction> {
 
@@ -21,7 +30,25 @@ public class TransactionController implements Controller<Transaction> {
     //TODO: Implement methods
 
     @Override
-    public void create(List<String> transaction) {
+    public void create(List<String> transactionToAdd) {
+
+        try {
+            int accountId = validateId(transactionToAdd.getFirst());
+            String transactionCategory = getCategoryByName(
+                    transactionToAdd.get(1)).getCategoryName();
+            LocalDate transactionDate = validateDate(transactionToAdd.get(2));
+            BigDecimal transactionAmount = validateAmount(transactionToAdd.get(3));
+
+            Transaction transaction = TransactionFactory.createTransaction(
+                    accountId, transactionCategory, transactionDate, transactionAmount);
+            transactionService.create(transaction);
+        } catch (IllegalArgumentException | IndexOutOfBoundsException | NullPointerException e) {
+            System.err.println("Invalid transaction data: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Error saving transaction: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.err.println("Error creating transaction: " + e.getMessage());
+        }
 
     }
 
