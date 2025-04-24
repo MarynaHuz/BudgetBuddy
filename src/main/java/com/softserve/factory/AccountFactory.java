6ca49109bc6af4d9dc4201e416ae832439b7a821
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-24 13:56:23 +0300

    refactor(account-factory): remove validation logic from factory
    Refactor `createAccount` to exclude validation logic, delegating input validation responsibility to other components. Simplifies the factory code and improves separation of concerns.

--- src/main/java/com/softserve/factory/AccountFactory.java
+++ src/main/java/com/softserve/factory/AccountFactory.java
@@ -5,26 +5,18 @@ import com.softserve.models.account.Currency;
 
 import java.math.BigDecimal;
 
-import static com.softserve.models.account.Currency.parseCurrencyCode;
-import static com.softserve.validators.AccountNameValidator.validateAccountName;
-import static com.softserve.validators.BalanceValidator.validateBalance;
-
 public class AccountFactory {
 
     private AccountFactory() {
     }
 
-    public static Account createAccount(String accountName, String currency,
-                                        String balance) {
-        String validatedName = validateAccountName(accountName);
-        Currency validatedCurrency = parseCurrencyCode(currency);
-        BigDecimal validatedBalance = validateBalance(balance);
-
+    public static Account createAccount(String accountName, Currency currency,
+                                        BigDecimal balance) {
         return Account.builder()
                 .accountId(0)
-                .accountName(validatedName)
-                .currency(validatedCurrency)
-                .balance(validatedBalance)
+                .accountName(accountName)
+                .currency(currency)
+                .balance(balance)
                 .build();
     }
 }
