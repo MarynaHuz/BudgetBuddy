3f5138d3746a5b56f8caca27330cc3e5c269b5f3
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-24 22:20:07 +0300

    refactor(transaction-factory): simplify transaction creation
    Remove redundant validation methods and update parameter types in `createTransaction` for cleaner and more efficient transaction creation logic. Simplified operations align better with existing data processing workflows.

--- src/main/java/com/softserve/factory/TransactionFactory.java
+++ src/main/java/com/softserve/factory/TransactionFactory.java
@@ -8,35 +8,28 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static com.softserve.utils.CategoryManager.getCategoryByName;
-import static com.softserve.validators.AmountValidator.validateAmount;
-import static com.softserve.validators.DateValidator.validateDate;
-import static com.softserve.validators.IdValidator.validateId;
 
 public class TransactionFactory {
 
     private TransactionFactory() {
     }
 
-    public static Transaction createTransaction(String accountId,
-                                                String transactionType,
-                                                String transactionDate,
-                                                String transactionAmount
+    public static Transaction createTransaction(int accountId,
+                                                String transactionCategory,
+                                                LocalDate transactionDate,
+                                                BigDecimal transactionAmount
     ) {
-        Category categoryByName = getCategoryByName(transactionType);
-
-        int validAccountId = validateId(accountId);
-        LocalDate validTransactionDate = validateDate(transactionDate);
+        Category categoryByName = getCategoryByName(transactionCategory);
         String categoryName = categoryByName.getCategoryName();
         String type = categoryByName.getTransactionType().getTypeName();
-        BigDecimal validatedTransactionAmount = validateAmount(transactionAmount);
 
         return Transaction.builder()
-                .accountId(validAccountId)
                 .transactionId(0)
-                .transactionDate(validTransactionDate)
+                .accountId(accountId)
                 .transactionType(type)
+                .transactionDate(transactionDate)
                 .category(categoryName)
-                .transactionAmount(validatedTransactionAmount)
+                .transactionAmount(transactionAmount)
                 .currency(Currency.UAH)
                 .build();
     }
