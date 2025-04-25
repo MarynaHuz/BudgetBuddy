d473cc09cba0a793392451719eec63c978e3ebdf
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-25 14:57:33 +0300

    refactor(transaction): clean up annotations and remove unused code
    Simplify the `Transaction` class by removing unused imports and annotations like `@ToString`, `@EqualsAndHashCode`, and `@JsonFormat`. Restricted `transactionId` setter access and removed the custom `setTransactionDate` method to reduce unnecessary complexity.

--- src/main/java/com/softserve/models/transaction/Transaction.java
+++ src/main/java/com/softserve/models/transaction/Transaction.java
@@ -1,34 +1,27 @@
 package com.softserve.models.transaction;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.softserve.models.account.Currency;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static com.softserve.validators.DateValidator.validateDate;
 
-@ToString
-@EqualsAndHashCode
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Transaction {
 
-    @Setter
+    @Setter(AccessLevel.NONE)
     private int transactionId;
     private int accountId;
     private String transactionType;
-    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate transactionDate;
     private String category;
     private BigDecimal transactionAmount;
     @Setter
     private Currency currency;
 
-    public void setTransactionDate(String transactionDate) {
-        this.transactionDate = validateDate(transactionDate);
-    }
 }
