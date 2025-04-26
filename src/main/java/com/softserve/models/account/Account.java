1179a43e69c8b380c0c2d46c0aba8754ebee9041
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-26 12:44:28 +0300

    refactor(account): simplify model by removing validation logic
    Removed inline validation and redundant setters from Account model to streamline the class and focus on core properties. This improves maintainability by delegating validation responsibilities to AccountController.

--- src/main/java/com/softserve/models/account/Account.java
+++ src/main/java/com/softserve/models/account/Account.java
@@ -5,31 +5,18 @@ import lombok.*;
 
 import java.math.BigDecimal;
 
-import static com.softserve.validators.AccountNameValidator.validateAccountName;
 
-
-@ToString
-@EqualsAndHashCode
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Account {
 
-    @Setter
     private int accountId;
     private String accountName;
     private Currency currency;
     @Positive
     private BigDecimal balance;
 
-    public void setAccountName(String accountName) {
-        this.accountName = validateAccountName(accountName);
-    }
-
-    public void setCurrency(Currency currency) {
-        //TODO: check if any transactions were added to this account.
-        // If they were added you can't change the currency due to problems with exchange rate
-        this.currency = currency;
-    }
 }
