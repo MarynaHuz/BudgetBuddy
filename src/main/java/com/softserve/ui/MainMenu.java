c44ade12d91e012e3430bee3aec5a08414f61a4f
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-27 12:29:44 +0300

    feat(main-menu): implement initial structure and interfaces
    Introduce Menu interface implementation in MainMenu class. Added constructors, fields, and basic setup for menu navigation to support account and transaction menus. Future enhancements will add functionality to the `show()` method.

--- src/main/java/com/softserve/ui/MainMenu.java
+++ src/main/java/com/softserve/ui/MainMenu.java
@@ -1,4 +1,26 @@
 package com.softserve.ui;
 
-public class MainMenu {
+import java.util.Map;
+import java.util.Scanner;
+
+public class MainMenu implements Menu{
+
+    private final Map<String, Menu> menus;
+    private final Scanner scanner = new Scanner(System.in);
+
+    private boolean exit;
+
+    public MainMenu(Menu accountMenu, Menu transactionMenu) {
+
+        this.menus = Map.of(
+                "1", accountMenu,
+                "2", transactionMenu
+        );
+    }
+
+
+    @Override
+    public void show() {
+
+    }
 }
