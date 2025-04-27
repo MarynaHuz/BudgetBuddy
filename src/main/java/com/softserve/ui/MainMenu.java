d5c8dbfd2b53faaa9b957b5e5ef893df0bf093b6
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-27 12:41:43 +0300

    feat(main-menu): add exit option and invalid choice handling
    Introduce an "Exit" option in the main menu to allow users to terminate the application gracefully. Add handling for invalid menu choices to improve user experience.

--- src/main/java/com/softserve/ui/MainMenu.java
+++ src/main/java/com/softserve/ui/MainMenu.java
@@ -3,7 +3,7 @@ package com.softserve.ui;
 import java.util.Map;
 import java.util.Scanner;
 
-public class MainMenu implements Menu{
+public class MainMenu implements Menu {
 
     private final Map<String, Menu> menus;
     private final Scanner scanner = new Scanner(System.in);
@@ -14,13 +14,33 @@ public class MainMenu implements Menu{
 
         this.menus = Map.of(
                 "1", accountMenu,
-                "2", transactionMenu
+                "2", transactionMenu,
+                "3", this::exitApplication
         );
     }
 
-
     @Override
     public void show() {
+        exit = false;
+        while (!exit) {
+            System.out.print("""
+                    ===== Budget Buddy =====
+                    1. Manage Accounts
+                    2. Manage Transactions
+                    3. Exit
+                    Choose an option:\s"""
+            );
+            String choice = scanner.nextLine();
+            menus.getOrDefault(choice, this::invalidChoice).show();
+        }
+    }
 
+    private void invalidChoice() {
+        System.out.println("Invalid choice. Please try again.");
+    }
+
+    private void exitApplication() {
+        System.out.println("Goodbye!");
+        exit = true;
     }
 }
