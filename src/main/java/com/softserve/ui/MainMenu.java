607fbda93df41de075228956e53f301b52eef0a5
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-27 17:51:45 +0300

    fix(ui): correct exit behavior and improve messaging
    Initialize `exit` variable to false to ensure proper application flow.
    Update exit messaging to include a more user-friendly and informative farewell.

--- src/main/java/com/softserve/ui/MainMenu.java
+++ src/main/java/com/softserve/ui/MainMenu.java
@@ -8,29 +8,28 @@ public class MainMenu implements Menu {
     private final Map<String, Menu> menus;
     private final Scanner scanner = new Scanner(System.in);
 
-    private boolean exit;
+    private boolean exit = false;
 
     public MainMenu(Menu accountMenu, Menu transactionMenu) {
 
         this.menus = Map.of(
+                "0", this::exitApplication,
                 "1", accountMenu,
-                "2", transactionMenu,
-                "3", this::exitApplication
+                "2", transactionMenu
         );
     }
 
     @Override
     public void show() {
-        exit = false;
         while (!exit) {
             System.out.print("""
-                    -------------------------------
-                    │   💰💵 𝔹𝕌𝔻𝔾𝔼𝕋 𝔹𝕌𝔻𝔻𝕐 💵💰   │
-                    -------------------------------
+                    --------------------------------
+                    │       💰 𝔹𝕌𝔻𝔾𝔼𝕋 𝔹𝕌𝔻𝔻𝕐        │
+                    --------------------------------
                     │ 1. 🏦 Manage Accounts        │
                     │ 2. 💳 Manage Transactions    │
                     │ 3. 🚪 Exit Application       │
-                    -------------------------------
+                    --------------------------------
                     Choose an option:\s"""
             );
             String choice = scanner.nextLine();
@@ -43,7 +42,15 @@ public class MainMenu implements Menu {
     }
 
     private void exitApplication() {
-        System.out.println("Goodbye!");
+        System.out.print("""
+                --------------------------------
+                │       👋 𝕋ℍ𝔸ℕ𝕂 𝕐𝕆𝕌!          │
+                --------------------------------
+                │    See you next time at      │
+                │        𝔹𝕌𝔻𝔾𝔼𝕋 𝔹𝕌𝔻𝔻𝕐!         │
+                --------------------------------
+                """
+        );
         exit = true;
     }
 }
