2ef0030de88e27cc809051a46f7d219cdc467835
Huz Maryna <maryna.huz.dev@gmail.com>
2025-04-30 11:36:08 +0300

    style(transaction-formatter): adjust spacing in transaction output template
    Improve alignment and formatting consistency in transaction output for better readability. No functional changes introduced.

--- src/main/java/com/softserve/formatters/TransactionFormatter.java
+++ src/main/java/com/softserve/formatters/TransactionFormatter.java
@@ -41,17 +41,17 @@ public class TransactionFormatter {
 
     public static String formatTransaction(Transaction transaction) {
         return """
-            ----------------------------------
-            │        💳 𝕋ℝ𝔸ℕ𝕊𝔸ℂ𝕋𝕀𝕆ℕ          │
-            ----------------------------------
-            │ 🔢 Transaction ID: %d
-            │ 🆔 Account ID: %d
-            │ 📤 Type: %s
-            │ 📅 Date: %s
-            │ 🏷️ Category: %s
-            │ 💰 Amount: %.2f %s
-            ----------------------------------
-            """.formatted(
+                ------------------------------------
+                │          💳 𝕋ℝ𝔸ℕ𝕊𝔸ℂ𝕋𝕀𝕆ℕ          │
+                ------------------------------------
+                │ 🔢 Transaction ID: %d
+                │ 🆔 Account ID: %d
+                │ 📤 Type: %s
+                │ 📅 Date: %s
+                │ 🏷️ Category: %s
+                │ 💰 Amount: %.2f %s
+                ------------------------------------
+                """.formatted(
                 transaction.getTransactionId(),
                 transaction.getAccountId(),
                 transaction.getTransactionType(),
